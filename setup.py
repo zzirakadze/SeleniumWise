760ee489404673324d4a7431a67e2aef2e113c95
Zura Zirakadze <zuraz@pango.co.il>
2022-10-27 19:14:56 +0400

    v5

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
 from setuptools import setup
 
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
 setup(
-    name="myapp",
-    version="0.1",
-    py_modules=["myapp"],
-    install_requires=["requirements.txt"],
+    name="SeleniumWise",
+    version="0.0.1",
+    description="Selenium Wrapper",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/zzirakadze/SeleniumWise.git",
+    author="Zura Zirakadze",
+    author_email="zirakadzez@gmail.com",
+    keywords="selenium, python",
+    license="MIT",
+    packages=['SeleniumWise'],
+    install_requires=['requirements.txt'],
+    include_package_data=True,
 )
